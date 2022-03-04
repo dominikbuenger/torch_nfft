8a61b7bb9c6ed4afe3f142fafde706ab951327d5
Dominik Bünger <dominik.buenger@gmail.com>
2022-03-03 16:44:28 -0800

    Enables multi-batch, multi-column transforms in ndft_adjoint

--- test/test_adjoint.py
+++ test/test_adjoint.py
@@ -18,23 +18,29 @@ from torch_nfft import nfft_adjoint, ndft_adjoint
 # N = 16
 # m = 4
 
-d = 3
+d = 2
+b = 3
 n = 1000
-pos = torch.rand((n,d), dtype=torch.float).cuda() - 0.5
+c = 10
+pos = torch.rand((n*b,d), dtype=torch.float).cuda() - 0.5
 pos /= 4*torch.linalg.norm(pos, dim=1, keepdim=True)
-x = torch.rand(n, dtype=torch.float).cuda()
-N = 32
-m = 6
+batch = None if b <= 1 else torch.div(torch.arange(n*b).cuda(), n, rounding_mode='trunc')
+x = torch.rand((n*b, c), dtype=torch.float).cuda()
+N = 16
+m = 4
 
-y_nfft = nfft_adjoint(x, pos, N=N, m=m)
+y_nfft = nfft_adjoint(x, pos, batch, N=N, m=m)
 # print("Fast:")
 # print(y_nfft[0])
+print("y_nfft shape: ", y_nfft.shape)
 
-y_ndft = ndft_adjoint(x, pos, N=N)
+# y_ndft = ndft_adjoint(x, pos, batch, N=N)
+y_ndft = torch.cat([ndft_adjoint(x[:,i], pos, batch, N=N)[...,None] for i in range(c)], dim=-1)
 # print("Exact:")
 # print(y_ndft)
+print("y_ndft shape: ", y_nfft.shape)
 
 print("Difference between fast and exact:")
-print("L1 norm:", torch.abs(y_nfft[0] - y_ndft).sum().item())
-print("L2 norm:", torch.linalg.norm(y_nfft[0] - y_ndft).item())
-print("Linf norm:", torch.abs(y_nfft[0] - y_ndft).max().item())
+print("L1 norm:", torch.abs(y_nfft - y_ndft).sum().item())
+print("L2 norm:", torch.linalg.norm(y_nfft - y_ndft).item())
+print("Linf norm:", torch.abs(y_nfft - y_ndft).max().item())
--- torch_nfft/__init__.py
+++ torch_nfft/__init__.py
@@ -21,13 +21,22 @@ def nfft_adjoint(x, pos, batch=None, N=16, m=3, real_output=False):
 
 
 # Non-approximatory reference implementation of NDFT
-def ndft_adjoint(x, pos, N=16):
+def ndft_adjoint(x, pos, batch=None, N=16):
     n, d = pos.shape
     device = pos.device
+    x = x.to(torch.cfloat)
 
     grid1d = torch.arange(-N/2, N/2, dtype=torch.float, device=device)
     grid = torch.cat([g[...,None] for g in torch.meshgrid(*((grid1d,)*d), indexing='ij')], dim=d)
 
-    fourier_tensor = torch.exp(2j * torch.pi * torch.tensordot(grid, pos, dims=([-1],[-1])))
+    def single_ndft(x_part, pos_part):
+        fourier_tensor = torch.exp(2j * torch.pi * torch.tensordot(grid, pos_part, dims=([-1],[-1])))
+        y = torch.tensordot(fourier_tensor, x_part, dims=([-1],[0]))
+        return y[None,...]
 
-    return torch.tensordot(fourier_tensor, x.to(torch.cfloat), dims=([-1],[0]))
+    if batch is None:
+        return single_ndft(x, pos)
+
+    else:
+        batch_size = batch.max().item() + 1
+        return torch.cat([single_ndft(x[batch == idx], pos[batch == idx]) for idx in range(batch_size)])
