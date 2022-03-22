bd0303f045a845da763384baeec6a39d4a1f00d9
Dominik Bünger <dominik.buenger@gmail.com>
2022-03-22 11:57:57 -0700

    Adds more extensive testing of the kernel interface

--- test/test_kernel.py
+++ test/test_kernel.py
@@ -9,7 +9,7 @@ dim = 2
 b = 2
 c = 3
 diameter = 10
-sigma = 0.2 * diameter
+relative_sigma = 1
 N = 16
 m = 3
 p = 0
@@ -21,15 +21,38 @@ batch = None if b <= 1 else torch.div(torch.arange(n*b).cuda(), n, rounding_mode
 x = torch.rand((n*b, c)).cuda()
 
 
-kernel = torch_nfft.GaussianKernel(sigma, dim, N, m, shift_by_center=True, max_infinity_norm=diameter/2, reg_degree=p)
+print("WITH ABSOLUTE SIGMA:")
 
+kernel = torch_nfft.GaussianKernel(relative_sigma*diameter,
+                                    dim, N, m, shift_by_center=True,
+                                    max_infinity_norm=diameter/2, reg_degree=p)
 y_approx = kernel(pos, batch=batch) @ torch.eye(n*b).cuda()
 
-y_true = torch_nfft.exact_gaussian_matrix(sigma, pos, batch=batch)
+y_true = torch_nfft.exact_gaussian_matrix(relative_sigma*diameter, pos, batch=batch)
 
 print("Approximate result:")
 print(y_approx)
 print("True result:")
 print(y_true)
+print("Relative maximum absolute error:", torch.abs(y_approx - y_true).max().item() / y_true.abs().max().item())
 
+
+print()
+print("WITH RELATIVE SIGMA:")
+
+kernel = torch_nfft.GaussianKernel(relative_sigma, dim, N, m, shift_by_center=True, reg_degree=p)
+y_approx = kernel(pos, batch=batch) @ torch.eye(n*b).cuda()
+
+pos_shifted_scaled = torch_nfft.utils.scale_points_by_norm(
+                        torch_nfft.utils.shift_points_by_center(
+                            pos, source_batch=batch)[0],
+                        source_batch=batch,
+                        norm="infinity" if p<0 else "euclidean")[0]
+
+y_true = torch_nfft.exact_gaussian_matrix(relative_sigma, pos_shifted_scaled, batch=batch)
+
+print("Approximate result:")
+print(y_approx)
+print("True result:")
+print(y_true)
 print("Relative maximum absolute error:", torch.abs(y_approx - y_true).max().item() / y_true.abs().max().item())
